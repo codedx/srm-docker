d8ac0d69c017aa11d49b6023a4bfc3c36f69003e
Steve Salas <ssalas@codedx.com>
2021-08-10 15:32:54 -0400

    chore: update version script for external db file

--- .version/version.ps1
+++ .version/version.ps1
@@ -8,7 +8,7 @@ Set-PSDebug -Strict
 $location = Join-Path $PSScriptRoot '..'
 Push-Location $location
 
-'./docker-compose.yml','./README.md' | ForEach-Object {
+'./docker-compose.yml','./docker-compose-external-db.yml','./README.md' | ForEach-Object {
 
 	$newContents = (Get-Content $_) -replace 'image:\scodedx/codedx-tomcat:v(\d+\.\d+\.\d+)', "image: codedx/codedx-tomcat:$codeDxVersionTag"
 	Set-Content $_	$newContents
